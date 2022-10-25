7aa2c136313c925d8ed27ed2800b3f9d7ad663ba
DESKTOP-L23VP1P\berka <berkayyaroglu1@gmail.com>
2022-10-25 17:52:59 +0300

    Palindrome

---
+++ Palindrome/src/den/Test.java
@@ -0,0 +1,24 @@
+package den;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String palindrome = "Do geese see God";
+		int len = palindrome.length();	
+		char[] tempCharArray = new char[len];
+		char[] charArray = new char[len];
+		
+		for(int i = 0; i < len; i++) {
+			tempCharArray[i] = palindrome.charAt(i);
+		}
+		
+		for(int j = 0; j < len; j++) {
+			charArray[j] = tempCharArray[len - 1 - j];
+		}
+		
+		String reversePalindrome = new String(charArray);
+		System.out.println(reversePalindrome);
+	}
+
+}
